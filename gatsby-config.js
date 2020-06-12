e21bd94e856b1db53e005af24fcf43b1ea6cbb4f
Rishabh Anand <rishabhanand42@gmail.com>
2020-06-13 00:32:53 +0530

    personalizing gatsby starter config  On branch master  Changes to be committed: 	modified:   gatsby-config.js 	modified:   package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rishabh's Portfolio`,
+    description: `Rishabh's Portfolio`,
+    author: `Rishabh Anand`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
