ebbc1f5b1d157a383f3b80089e68fe8dba2bb6ea
Rishabh Anand <rishabhanand42@gmail.com>
2020-07-03 12:27:06 +0530

    mail feature added  Changes to be committed: 	modified:   package.json 	modified:   src/components/Contact/Form.js 	modified:   src/components/Contact/style.css 	new file:   src/config/axios.js

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import Button from '../Button';
+import React, { useState } from "react"
+import Button from "../Button"
+import API from "../../config/axios";
 
 export default function (props) {
+  const [email, setEmail] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [isError, setIsError] = useState(false)
   return (
     <form className="contact-form">
       {/* <h2 className="form-heading">Contact Me</h2> */}
       <div className="form-group">
-        <input type="text" placeholder="Email" />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <Button onClick={(e) => {
-          e.preventDefault();
-        }}>Submit</Button>
+        <Button
+          onClick={e => {
+            e.preventDefault()
+            setIsLoading(true)
+            API.post("/mail/send", { client_email: email }, {})
+              .then(() => {
+                setIsSuccess(true)
+                setIsError(false)
+                setIsLoading(false)
+              })
+              .catch(() => {
+                setIsSuccess(false)
+                setIsError(true)
+                setIsLoading(false)
+              })
+          }}
+        >
+          Submit
+        </Button>
+        {isSuccess && <h6 className="contact-funny">Mail sent</h6>}
       </div>
     </form>
-  );
+  )
 }
---
+++ src/config/axios.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export default axios.create({baseURL: 'https://backend.rishabh-anand.com/api/v1'});
